47578f020d3fdfb370a06914099628ee1e55f989
Mohammad AL Mahamid <mohammad.almahamid@exxeta.com>
2025-02-26 09:40:47 +0100

    more Views

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, World, You are at the poll index")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." %question_id)
+
+
+def results(request, question_id):
+    response= "You're looking at results if question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." %question_id)
