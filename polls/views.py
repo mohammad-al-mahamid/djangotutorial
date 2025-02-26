d033b57927b8d11bf225f6abfbf53bd511885eec
Mohammad AL Mahamid <mohammad.almahamid@exxeta.com>
2025-02-26 09:59:05 +0100

    first template and load data from db

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,17 @@
+from django.template import loader
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from polls.models import Question
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World, You are at the poll index")
+    latest_question_list= Question.objects.order_by("-pub_date")[:5]
+    template= loader.get_template("polls/index.html")
+    context = {
+        "latest_question_list": latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, question_id):
